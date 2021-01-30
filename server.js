713a2ccc1e6bff2efd6eab40e75df8040703dc03
Shloka Jain <shlok2598tech@gmail.com>
2021-01-30 09:16:11 +0530

    Register complete

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// routes
+
+app.use("/api/user", require("./routes/auth"));
+
+// DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err || !client) {
+      console.log(err);
+    } else {
+      console.log(`Database connected!!`);
+    }
+  }
+);
+
+// PORT
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port:${PORT}`);
