84ba333e749050d619d40070c861b8f334f7113b
Shloka Jain <shlok2598tech@gmail.com>
2021-02-01 15:01:56 +0530

    make it better

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
+
+const data = require("./data.json");
 
 // middlewares
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-// routes
-
-app.use("/api/user", require("./routes/auth"));
-
-// DB
-mongoose.connect(
-  process.env.DB_CONNECTION,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    if (err || !client) {
-      console.log(err);
-    } else {
-      console.log(`Database connected!!`);
+app.post("/api/message", (req, res) => {
+  const { message } = req.body;
+  if (!message) {
+    return res.status(400).json({
+      success: false,
+      error: "Please do not leave the input field empty!!",
+    });
+  } else {
+    const resMsg = data.filter((q) => {
+      return q.question.match(req.body.message);
+    });
+    if (!resMsg.length) {
+      res.json({ result: "Did not get what did you say ? Please try again" });
     }
+    res.json({ result: resMsg[0] });
   }
-);
+});
 
 // PORT
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port:${PORT}`);
+  console.log(`Ready for Q & A`);
 });
